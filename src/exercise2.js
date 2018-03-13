45c28b74ecb46d8bc3eb89d94f2c4adecf1b843a
Evan <evan1998@hotmail.fr>
2018-03-13 10:44:36 +0100

    Exercice 1 & 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,23 @@
 var mapCustomer = function(customer) {
-  // Your future job begins here ...
+    var input = customer
+    function emptyobject (input){//fonction empty ne marche pas
+        if (input === null){
+            return true;
+        }
+        if (input.length === 0){
+            return true;
+        }
+        return true;
+    }
+    if (emptyobject(input) === true) {
+        input = {
+      firstname: 'John',
+      lastname: 'Doe',
+      products: [],
+      moneySpent: 0
+    }
+}
+    return input;
 }
 
 module.exports = {
--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,28 @@
-var applyMap = function() {
-  // Your future job begins here ...
+
+function map1(value) {
+  return value + 10
+}
+function map2(value) {
+  return value * 3
+}
+var inputMaps = [map1, map2]
+
+var applyMap = function(inputValues, inputMaps) {
+  if (inputValues === undefined || inputMaps === undefined) {
+    var output = [];
+    return output;
+  }
+  else {
+    var output = [];
+    var mapping = [];
+    for (var i = 0; i < inputMaps.length; i++) {
+      mapping = inputValues.map(inputMaps[i]);
+      output.push(mapping);
+    }
+    return output;
+   
+  }
+  
 }
 
 module.exports = {
